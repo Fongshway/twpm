ad7f8b1845cdd6a2162eb184825f9100e092de31
Fongshway <kmf78@georgetown.edu>
2018-10-14 18:30:52 -0700

    save

--- twpm/hook_runner.py
+++ twpm/hook_runner.py
@@ -1,10 +1,10 @@
-from twpm.hooks.test_hook import main
+from twpm.hooks import test_hook
 
 
 def run(event):
     # Load task and taskwarrior instance
 
     # Run all active hooks
-    main()
+    test_hook.main()
 
     # Export the final task after all active hooks have run
