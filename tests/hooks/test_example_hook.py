188eb05a51d03f5ce1e4fa6c81c77c52baecf99b
Fongshway <8451964+Fongshway@users.noreply.github.com>
2018-10-15 02:09:45 -0400

    Add test for example hook (#6)

---
+++ tests/hooks/test_example_hook.py
@@ -0,0 +1,10 @@
+from twpm.hooks import example_hook
+
+
+def test_example_hook():
+    test_task = {"status": "pending", "description": "Fix tw-98765", "tags": "in", "modified": "20181015T054805Z",
+                 "entry": "20181015T054805Z", "uuid": "cee8cefa-0b9d-432c-a7da-cd68f50466bf"}
+
+    example_hook.main(test_task)
+
+    assert test_task['description'] == "Fix https://github.com/GothenburgBitFactory/taskwarrior/issues/tw-98765"
