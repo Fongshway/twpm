e8c65c5206329e4143599dce8f5cf56fe07618a4
Fongshway <kmf78@georgetown.edu>
2018-10-15 01:34:17 -0400

    Refactor hook runner (#5)

--- twpm/hook_runner.py
+++ twpm/hook_runner.py
@@ -1,6 +1,14 @@
 from twpm.hooks import example_hook
 
 
+def on_add_runner():
+    run('on_add')
+
+
+def on_modify_runner():
+    run('on_modify')
+
+
 def run(event):
     # Load task and taskwarrior instance
 
--- twpm/on_modify_twpm.py
+++
@@ -1,5 +0,0 @@
-from twpm import hook_runner
-
-
-def main():
-    hook_runner.run('on_modify')
