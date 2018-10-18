1af3f27b10d8f2a715aec1b7769311081fe5ba54
Fongshway <kmf78@georgetown.edu>
2018-10-17 23:50:58 -0700

    update test

--- tests/test_hook_runner.py
+++ tests/test_hook_runner.py
@@ -5,12 +5,12 @@ def test_to_output():
     serialized_task = {
         'status': 'pending',
         'description': 'Fix tw-98765',
-        'tags': ['in'],
+        'tags': ['in', 'next'],
         'modified': '20181018T050328Z',
         'entry': '20181018T050328Z',
         'uuid': 'd1b29100-3ee1-462e-b59e-4b570398b2d6'
     }
-    expected = '{"status":"pending","description":"Fix tw-98765","tags":"in","modified":"20181018T050328Z",' \
+    expected = '{"status":"pending","description":"Fix tw-98765","tags":"in,next","modified":"20181018T050328Z",' \
                '"entry":"20181018T050328Z","uuid":"d1b29100-3ee1-462e-b59e-4b570398b2d6"}'
 
     result = hook_runner.to_output(serialized_task)
