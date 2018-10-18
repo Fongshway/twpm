ad4c887270570ff4ae470621b0f5327a8ec88d74
Fongshway <kmf78@georgetown.edu>
2018-10-18 03:17:28 -0400

    Use taskw library to handle tasks in hooks (#11)

--- tests/hooks/test_example_hook.py
+++ tests/hooks/test_example_hook.py
@@ -1,9 +1,11 @@
+from taskw.task import Task
+
 from twpm.hooks import example_hook
 
 
 def test_example_hook():
-    test_task = {"status": "pending", "description": "Fix tw-98765", "tags": "in", "modified": "20181015T054805Z",
-                 "entry": "20181015T054805Z", "uuid": "cee8cefa-0b9d-432c-a7da-cd68f50466bf"}
+    test_task = Task({"status": "pending", "description": "Fix tw-98765", "tags": "in", "modified": "20181015T054805Z",
+                 "entry": "20181015T054805Z", "uuid": "cee8cefa-0b9d-432c-a7da-cd68f50466bf"})
 
     example_hook.main(test_task)
 
--- twpm/hook_runner.py
+++ twpm/hook_runner.py
@@ -1,6 +1,10 @@
 import json
 import sys
 
+from taskw import TaskWarrior
+from taskw.fields import ArrayField
+from taskw.task import Task
+
 from twpm.hooks import example_hook
 
 
@@ -12,15 +16,30 @@ def on_modify_runner():
     run('on_modify')
 
 
+def to_output(task: dict) -> str:
+    """
+    Convert serialized task representation to hook output JSON
+    """
+    fields = Task.FIELDS.copy()
+
+    for k, v in task.items():
+        if isinstance(fields[k], ArrayField):
+            task[k] = ','.join(v)
+
+    return json.dumps(task, separators=(',', ':'))
+
+
 def run(event):
     # Load task and Taskwarrior instance
-    hook_task = json.loads(sys.stdin.readline())
+    tw = TaskWarrior()
+    udas = tw.config.get_udas()
+    hook_task = Task(json.loads(sys.stdin.readline()), udas)
 
     # Run all active hooks
     example_hook.main(hook_task)
 
     # Export the final task after all active hooks have run
-    print(json.dumps(hook_task))
+    print(to_output(hook_task.serialized()))
 
     # Exit
     sys.exit(0)
