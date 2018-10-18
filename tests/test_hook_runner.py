87820b36741cc59db37630fa35838e77b03f7758
Fongshway <kmf78@georgetown.edu>
2018-10-17 23:36:47 -0700

    refactor

--- tests/test_hook_runner.py
+++ tests/test_hook_runner.py
@@ -2,12 +2,16 @@ from twpm import hook_runner
 
 
 def test_to_output():
-    input = {'status': 'pending', 'description': 'Fix tw-98765',
-     'tags': ['in'], 'modified': '20181018T050328Z', 'entry': '20181018T050328Z',
-     'uuid': 'd1b29100-3ee1-462e-b59e-4b570398b2d6'}
+    serialized_task = {
+        'status': 'pending',
+        'description': 'Fix tw-98765',
+        'tags': ['in'],
+        'modified': '20181018T050328Z',
+        'entry': '20181018T050328Z',
+        'uuid': 'd1b29100-3ee1-462e-b59e-4b570398b2d6'
+    }
     expected = '{"status":"pending","description":"Fix tw-98765","tags":"in","modified":"20181018T050328Z","entry":"20181018T050328Z","uuid":"d1b29100-3ee1-462e-b59e-4b570398b2d6"}'
 
-    result = hook_runner.to_output(input)
+    result = hook_runner.to_output(serialized_task)
 
     assert result == expected
-
