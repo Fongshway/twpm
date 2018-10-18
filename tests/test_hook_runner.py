d0285f88b5606bead95734f8a1482f82e2ccbd61
Fongshway <8451964+Fongshway@users.noreply.github.com>
2018-10-18 03:17:28 -0400

    Use taskw library to handle tasks in hooks (#11)

---
+++ tests/test_hook_runner.py
@@ -0,0 +1,18 @@
+from twpm import hook_runner
+
+
+def test_to_output():
+    serialized_task = {
+        'status': 'pending',
+        'description': 'Fix tw-98765',
+        'tags': ['in', 'next'],
+        'modified': '20181018T050328Z',
+        'entry': '20181018T050328Z',
+        'uuid': 'd1b29100-3ee1-462e-b59e-4b570398b2d6'
+    }
+    expected = '{"status":"pending","description":"Fix tw-98765","tags":"in,next","modified":"20181018T050328Z",' \
+               '"entry":"20181018T050328Z","uuid":"d1b29100-3ee1-462e-b59e-4b570398b2d6"}'
+
+    result = hook_runner.to_output(serialized_task)
+
+    assert result == expected
