bb49c2fae3193d59af0e72eed11c756050c06ecc
Fongshway <kmf78@georgetown.edu>
2018-10-21 14:47:10 -0700

    Add yapf style checks (#12)

--- tests/hooks/test_example_hook.py
+++ tests/hooks/test_example_hook.py
@@ -4,8 +4,16 @@ from twpm.hooks import example_hook
 
 
 def test_example_hook():
-    test_task = Task({"status": "pending", "description": "Fix tw-98765", "tags": "in", "modified": "20181015T054805Z",
-                 "entry": "20181015T054805Z", "uuid": "cee8cefa-0b9d-432c-a7da-cd68f50466bf"})
+    test_task = Task(
+        {
+            "status": "pending",
+            "description": "Fix tw-98765",
+            "tags": "in",
+            "modified": "20181015T054805Z",
+            "entry": "20181015T054805Z",
+            "uuid": "cee8cefa-0b9d-432c-a7da-cd68f50466bf"
+        }
+    )
 
     example_hook.main(test_task)
 
