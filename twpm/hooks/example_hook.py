4767402bc3c7124690931bbc41b8d6a8dac8b0d2
Fongshway <kmf78@georgetown.edu>
2018-10-21 14:37:25 -0700

    yapf refactor

--- twpm/hooks/example_hook.py
+++ twpm/hooks/example_hook.py
@@ -8,10 +8,12 @@ from taskw.task import Task
 def main(task: Task):
     print()
     original = task['description']
-    task['description'] = re.sub(r'\b(tw-\d+)\b',
-                                 r'https://github.com/GothenburgBitFactory/taskwarrior/issues/\1',
-                                 original,
-                                 flags=re.IGNORECASE)
+    task['description'] = re.sub(
+        r'\b(tw-\d+)\b',
+        r'https://github.com/GothenburgBitFactory/taskwarrior/issues/\1',
+        original,
+        flags=re.IGNORECASE
+    )
 
     if original != task['description']:
         print('Link added')
