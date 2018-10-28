98d4e92bbb96ac0bb5722e013167a6e7d505a07d
Fongshway <kmf78@georgetown.edu>
2018-10-28 16:10:43 -0700

    Add test for from_input

--- tests/test_hook_runner.py
+++ tests/test_hook_runner.py
@@ -2,9 +2,50 @@
 """
 Hook runner tests
 """
+import datetime
+import uuid
 from copy import deepcopy
 
+import six
+from dateutil.tz import tzutc
+
 from twpm.hook_runner import HookRunner
+
+
+def test_from_input(tw):
+    input_add_data = six.StringIO(
+        '{'
+        '"description":"Go to Camelot",'
+        '"entry":"20180618T030242Z",'
+        '"status":"pending",'
+        '"start":"20181012T110605Z",'
+        '"uuid":"daa3ff05-f716-482e-bc35-3e1601e50778"'
+        '}')
+    input_modify_data = six.StringIO(
+        '\n'.join([
+            input_add_data.getvalue(),
+            (
+                '{'
+                '"description":"Go to Camelot again",'
+                '"entry":"20180618T030242Z",'
+                '"status":"pending",'
+                '"start":"20181012T110605Z",'
+                '"uuid":"daa3ff05-f716-482e-bc35-3e1601e50778"'
+                '}'
+            ),
+        ]),
+    )
+    on_add_runner = HookRunner('on_add', tw)
+    on_add_task_actual = on_add_runner.from_input(input_add_data)
+    assert on_add_task_actual['description'] == "Go to Camelot"
+    assert on_add_task_actual['entry'] == datetime.datetime(2018, 6, 18, 3, 2, 42, tzinfo=tzutc())
+    assert on_add_task_actual['status'] == "pending"
+    assert on_add_task_actual['start'] == datetime.datetime(2018, 10, 12, 11, 6, 5, tzinfo=tzutc())
+    assert on_add_task_actual['uuid'] == uuid.UUID("daa3ff05-f716-482e-bc35-3e1601e50778")
+
+    on_modify_runner = HookRunner('on_modify', tw)
+    on_modify_task_actual = on_modify_runner.from_input(input_modify_data)
+    assert on_modify_task_actual['description'] == "Go to Camelot again"
 
 
 def test_to_output(tw):
