8c5405d2757fcf92d3053306c5aa3be4b2d164e7
Fongshway <kmf78@georgetown.edu>
2018-10-28 14:56:49 -0700

    fixup

--- tests/test_hook_runner.py
+++ tests/test_hook_runner.py
@@ -16,7 +16,7 @@ def test_to_output(tw):
         'entry': '20181018T050328Z',
         'uuid': 'd1b29100-3ee1-462e-b59e-4b570398b2d6'
     }
-    expected = '{"status":"pending","description":"Fix tw-98765","tags":"in,next","modified":"20181018T050328Z",' \
+    expected_output = '{"status":"pending","description":"Fix tw-98765","tags":"in,next","modified":"20181018T050328Z",' \
                '"entry":"20181018T050328Z","uuid":"d1b29100-3ee1-462e-b59e-4b570398b2d6"}'
 
     on_add_runner = HookRunner('on_add', tw)
@@ -25,5 +25,5 @@ def test_to_output(tw):
     on_add_result = on_add_runner.to_output(deepcopy(serialized_task))
     on_modify_result = on_modify_runner.to_output(deepcopy(serialized_task))
 
-    assert on_add_result == expected
-    assert on_modify_result == expected
+    assert on_add_result == expected_output
+    assert on_modify_result == expected_output
