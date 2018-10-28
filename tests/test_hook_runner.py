2d3fdff141a5447b4f1cc151d6943a57753a6b67
Fongshway <kmf78@georgetown.edu>
2018-10-28 16:29:03 -0700

    pylint

--- tests/test_hook_runner.py
+++ tests/test_hook_runner.py
@@ -60,8 +60,9 @@ def test_to_output(tw):
         'entry': '20181018T050328Z',
         'uuid': 'd1b29100-3ee1-462e-b59e-4b570398b2d6'
     }
-    expected_output = '{"status":"pending","description":"Fix tw-98765","tags":"in,next","modified":"20181018T050328Z",' \
-               '"entry":"20181018T050328Z","uuid":"d1b29100-3ee1-462e-b59e-4b570398b2d6"}'
+    expected_output = '{"status":"pending","description":"Fix tw-98765","tags":"in,next",' \
+                      '"modified":"20181018T050328Z","entry":"20181018T050328Z",' \
+                      '"uuid":"d1b29100-3ee1-462e-b59e-4b570398b2d6"}'
 
     on_add_runner = HookRunner('on_add', tw)
     on_modify_runner = HookRunner('on_modify', tw)
