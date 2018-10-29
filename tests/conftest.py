e5d7defb66a57a077e27a9b4e86f6e78558bc24f
Fongshway <8451964+Fongshway@users.noreply.github.com>
2018-10-28 22:03:47 -0700

    Refactor hook_runner functions into class (#17)

---
+++ tests/conftest.py
@@ -0,0 +1,26 @@
+# pylint: disable=redefined-outer-name
+"""
+Pytest fixtures
+"""
+import os
+import shutil
+import tempfile
+
+import pytest
+from taskw import TaskWarrior
+
+
+@pytest.fixture
+def default_taskrc_path():
+    """ Path to default taskrc resource """
+    path_to_default_taskrc = os.path.join(os.path.dirname(__file__), 'resources/default.taskrc')
+    yield path_to_default_taskrc
+
+
+@pytest.fixture
+def tw(default_taskrc_path):
+    """ TaskWarrior instance that uses default.taskrc"""
+    taskdata = tempfile.mkdtemp()
+    tw = TaskWarrior(config_filename=default_taskrc_path, config_overrides={'data.location': taskdata})
+    yield tw
+    shutil.rmtree(taskdata)
