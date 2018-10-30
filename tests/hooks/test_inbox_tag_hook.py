7c4c5696ec0583f7f0ccea7b03f1e830408c31c4
Fongshway <kmf78@georgetown.edu>
2018-10-29 20:46:58 -0700

    Add hook for inbox tags (#18)

---
+++ tests/hooks/test_inbox_tag_hook.py
@@ -0,0 +1,43 @@
+# pylint: disable=missing-docstring
+"""
+Inbox tag hook tests
+"""
+from taskw.task import Task
+
+from twpm.hooks import inbox_tag_hook
+
+
+def test_inbox_tag_hook_add():
+    test_task = Task(
+        {
+            "description": "test task 1",
+            "status": "pending",
+            "tags": ["tag1", "tag2"],
+            "modified": "20181015T054805Z",
+            "entry": "20181015T054805Z",
+            "uuid": "cee8cefa-0b9d-432c-a7da-cd68f50466bf"
+        }
+    )
+
+    inbox_tag_hook.main(test_task)
+
+    assert test_task['description'] == "test task 1"
+    assert test_task['tags'] == ["tag1", "tag2", "in"]
+
+
+def test_inbox_tag_hook_remove():
+    test_task = Task(
+        {
+            "description": "test task 2",
+            "status": "pending",
+            "tags": ["@context", "tag1", "in"],
+            "modified": "20181015T054805Z",
+            "entry": "20181015T054805Z",
+            "uuid": "cee8cefa-0b9d-432c-a7da-cd68f50466bf"
+        }
+    )
+
+    inbox_tag_hook.main(test_task)
+
+    assert test_task['description'] == "test task 2"
+    assert test_task['tags'] == ["@context", "tag1"]
