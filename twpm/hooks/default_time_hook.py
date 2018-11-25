afbb41471518dd5d04011f685d682e3fcbc20c7c
Fongshway <8451964+Fongshway@users.noreply.github.com>
2018-11-24 18:48:31 -0800

    pylint

--- twpm/hooks/default_time_hook.py
+++ twpm/hooks/default_time_hook.py
@@ -13,6 +13,12 @@ DEFAULT_TIME = time(23, 59, 59)  # Your wanted default time
 
 
 def set_default_time(timestamp: datetime) -> datetime:
+    """
+    Helper function to set the default timestamp for a given datetime.
+
+    :param timestamp:
+    :return: datetime with hour, minute, and second values to set the defaults.
+    """
     return timestamp.replace(
         hour=DEFAULT_TIME.hour,
         minute=DEFAULT_TIME.minute,
