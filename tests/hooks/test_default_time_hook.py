cb88f8964177dcf05f0f2e305c7357702262183b
Fongshway <8451964+Fongshway@users.noreply.github.com>
2018-11-25 00:47:45 -0800

    Update default time hook to properly check for midnight in local time (#25)

--- tests/hooks/test_default_time_hook.py
+++ tests/hooks/test_default_time_hook.py
@@ -3,54 +3,69 @@
 Default time hook tests
 """
 import uuid
-from datetime import datetime, timedelta
+from datetime import datetime
+from datetime import timedelta
 
+import pytz
+from dateutil import tz
 from dateutil.tz import tzutc
 from taskw.task import Task
+from taskw.utils import DATE_FORMAT
 
 from twpm.hooks import default_time_hook
 from twpm.hooks.default_time_hook import DEFAULT_TIME
 
-NOW = datetime.now().replace(tzinfo=tzutc())
+LOCAL_TZ = tz.tzlocal()
+NOW = datetime.now()
 
 
 def test_default_time_midnight():
-    due_date = NOW.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=2)
-    wait_date = NOW.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
+    due_date = NOW.replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=LOCAL_TZ) + timedelta(days=2)
+    wait_date = NOW.replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=LOCAL_TZ) + timedelta(days=1)
+
     test_task = Task(
         {
             "status": "pending",
             "description": "Super urgent test task",
             "tags": ["@work"],
-            "modified": NOW.strftime("%Y%m%dT%H%M%SZ"),
-            "entry": NOW.strftime("%Y%m%dT%H%M%SZ"),
-            "due": due_date.strftime("%Y%m%dT%H%M%SZ"),
-            "wait": wait_date.strftime("%Y%m%dT%H%M%SZ"),
+            "modified": NOW.strftime(DATE_FORMAT),
+            "entry": NOW.strftime(DATE_FORMAT),
+            "due": due_date.astimezone(pytz.utc).strftime(DATE_FORMAT),
+            "wait": wait_date.astimezone(pytz.utc).strftime(DATE_FORMAT),
             "uuid": str(uuid.uuid4())
         }
     )
     default_time_hook.main(test_task)
-    expected_due = due_date.replace(hour=DEFAULT_TIME.hour, minute=DEFAULT_TIME.minute, second=DEFAULT_TIME.second)
-    expected_wait = wait_date.replace(hour=DEFAULT_TIME.hour, minute=DEFAULT_TIME.minute, second=DEFAULT_TIME.second)
-    assert test_task['due'] == expected_due
-    assert test_task['wait'] == expected_wait
+    assert test_task['due'] == due_date.replace(
+        hour=DEFAULT_TIME.hour,
+        minute=DEFAULT_TIME.minute,
+        second=DEFAULT_TIME.second,
+        tzinfo=tzutc(),
+    )
+    assert test_task['wait'] == wait_date.replace(
+        hour=DEFAULT_TIME.hour,
+        minute=DEFAULT_TIME.minute,
+        second=DEFAULT_TIME.second,
+        tzinfo=tzutc(),
+    )
 
 
 def test_default_time_not_midnight():
     due_date = NOW.replace(second=1, microsecond=0) + timedelta(days=2)
     wait_date = NOW.replace(second=1, microsecond=0) + timedelta(days=1)
+
     test_task = Task(
         {
             "status": "pending",
             "description": "Super urgent test task",
             "tags": ["@work"],
-            "modified": NOW.strftime("%Y%m%dT%H%M%SZ"),
-            "entry": NOW.strftime("%Y%m%dT%H%M%SZ"),
-            "due": due_date.strftime("%Y%m%dT%H%M%SZ"),
-            "wait": wait_date.strftime("%Y%m%dT%H%M%SZ"),
+            "modified": NOW.strftime(DATE_FORMAT),
+            "entry": NOW.strftime(DATE_FORMAT),
+            "due": due_date.strftime(DATE_FORMAT),
+            "wait": wait_date.strftime(DATE_FORMAT),
             "uuid": str(uuid.uuid4())
         }
     )
     default_time_hook.main(test_task)
-    assert test_task['due'] == due_date
-    assert test_task['wait'] == wait_date
+    assert test_task['due'] == due_date.replace(tzinfo=tzutc())
+    assert test_task['wait'] == wait_date.replace(tzinfo=tzutc())
--- twpm/hooks/default_time_hook.py
+++ twpm/hooks/default_time_hook.py
@@ -5,11 +5,22 @@ import logging
 from datetime import datetime
 from datetime import time
 
+from dateutil import tz
 from taskw.task import Task
 
 logger = logging.getLogger(__name__)
 
 DEFAULT_TIME = time(23, 59, 59)  # Your wanted default time
+
+
+def is_local_midnight(timestamp: datetime) -> bool:
+    """
+    Helper function to evaluate whether or not a dateime is midnight in local time.
+
+    :param timestamp:
+    :return: Boolean indicating if the datetime is midnight in local time.
+    """
+    return timestamp.astimezone(tz.tzlocal()).time() == time(0, 0, 0)
 
 
 def set_default_time(timestamp: datetime) -> datetime:
@@ -41,16 +52,16 @@ def main(task: Task) -> None:
     if not task_due_date:
         return
 
-    if task_due_date.time() == time(0, 0, 0):
+    if task_due_date.time() and is_local_midnight(task_due_date):
         task['due'] = set_default_time(task['due'])
         logger.info("Default due time has been set to %s", task['due'])
 
-    task_due_wait = task.get('wait', None)
+    task_wait_date = task.get('wait', None)
 
     # Exit hook if task has no wait date
-    if not task_due_wait:
+    if not task_wait_date:
         return
 
-    if task_due_wait.time() == time(0, 0, 0):
+    if task_wait_date.time() and is_local_midnight(task_wait_date):
         task['wait'] = set_default_time(task['wait'])
         logger.info("Default wait time has been set to %s", task['wait'])
