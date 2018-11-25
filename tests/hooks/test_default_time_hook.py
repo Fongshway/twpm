6b9c3a31ed0110c46d96591f24c309a4f28c8f11
Fongshway <8451964+Fongshway@users.noreply.github.com>
2018-11-24 18:56:59 -0800

    Add extra test

--- tests/hooks/test_default_time_hook.py
+++ tests/hooks/test_default_time_hook.py
@@ -14,7 +14,7 @@ from twpm.hooks.default_time_hook import DEFAULT_TIME
 NOW = datetime.now().replace(tzinfo=tzutc())
 
 
-def test_default_time_hook():
+def test_default_time_hook_midnight():
     due_date = NOW.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=2)
     wait_date = NOW.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
     test_task = Task(
@@ -34,3 +34,23 @@ def test_default_time_hook():
     expected_wait = wait_date.replace(hour=DEFAULT_TIME.hour, minute=DEFAULT_TIME.minute, second=DEFAULT_TIME.second)
     assert test_task['due'] == expected_due
     assert test_task['wait'] == expected_wait
+
+
+def test_default_time_hook_not_midnight():
+    due_date = NOW.replace(second=1, microsecond=0) + timedelta(days=2)
+    wait_date = NOW.replace(second=1, microsecond=0) + timedelta(days=1)
+    test_task = Task(
+        {
+            "status": "pending",
+            "description": "Super urgent test task",
+            "tags": ["@work"],
+            "modified": NOW.strftime("%Y%m%dT%H%M%SZ"),
+            "entry": NOW.strftime("%Y%m%dT%H%M%SZ"),
+            "due": due_date.strftime("%Y%m%dT%H%M%SZ"),
+            "wait": wait_date.strftime("%Y%m%dT%H%M%SZ"),
+            "uuid": str(uuid.uuid4())
+        }
+    )
+    default_time_hook.main(test_task)
+    assert test_task['due'] == due_date
+    assert test_task['wait'] == wait_date
