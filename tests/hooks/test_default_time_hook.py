bb8381708354cbb224dfe236036f71015f98d7dd
Fongshway <8451964+Fongshway@users.noreply.github.com>
2018-11-24 18:27:40 -0800

    refactor

--- tests/hooks/test_default_time_hook.py
+++ tests/hooks/test_default_time_hook.py
@@ -9,11 +9,13 @@ from dateutil.tz import tzutc
 from taskw.task import Task
 
 from twpm.hooks import default_time_hook
+from twpm.hooks.default_time_hook import DEFAULT_TIME
 
 NOW = datetime.now().replace(tzinfo=tzutc())
 
 
 def test_default_time_hook():
+    due_date = NOW.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=2)
     test_task = Task(
         {
             "status": "pending",
@@ -21,9 +23,10 @@ def test_default_time_hook():
             "tags": ["@work"],
             "modified": NOW.strftime("%Y%m%dT%H%M%SZ"),
             "entry": NOW.strftime("%Y%m%dT%H%M%SZ"),
-            "due": (NOW.replace(hour=0, minute=0, second=0) + timedelta(days=2)).strftime("%Y%m%dT%H%M%SZ"),
+            "due": due_date.strftime("%Y%m%dT%H%M%SZ"),
             "uuid": str(uuid.uuid4())
         }
     )
     default_time_hook.main(test_task)
-    assert test_task['due'] == datetime(2018, 11, 26, 23, 59, 59, tzinfo=tzutc())
+    expected_due_date = due_date.replace(hour=DEFAULT_TIME.hour, minute=DEFAULT_TIME.minute, second=DEFAULT_TIME.second)
+    assert test_task['due'] == expected_due_date
