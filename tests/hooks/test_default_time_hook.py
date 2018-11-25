ca478914f0edfcbe256445e4fe86850fd81515bb
Fongshway <8451964+Fongshway@users.noreply.github.com>
2018-11-24 19:11:04 -0800

    Add hook that sets default timestamps for dates (#21)

---
+++ tests/hooks/test_default_time_hook.py
@@ -0,0 +1,56 @@
+# pylint: disable=missing-docstring
+"""
+Default time hook tests
+"""
+import uuid
+from datetime import datetime, timedelta
+
+from dateutil.tz import tzutc
+from taskw.task import Task
+
+from twpm.hooks import default_time_hook
+from twpm.hooks.default_time_hook import DEFAULT_TIME
+
+NOW = datetime.now().replace(tzinfo=tzutc())
+
+
+def test_default_time_midnight():
+    due_date = NOW.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=2)
+    wait_date = NOW.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
+    test_task = Task(
+        {
+            "status": "pending",
+            "description": "Super urgent test task",
+            "tags": ["@work"],
+            "modified": NOW.strftime("%Y%m%dT%H%M%SZ"),
+            "entry": NOW.strftime("%Y%m%dT%H%M%SZ"),
+            "due": due_date.strftime("%Y%m%dT%H%M%SZ"),
+            "wait": wait_date.strftime("%Y%m%dT%H%M%SZ"),
+            "uuid": str(uuid.uuid4())
+        }
+    )
+    default_time_hook.main(test_task)
+    expected_due = due_date.replace(hour=DEFAULT_TIME.hour, minute=DEFAULT_TIME.minute, second=DEFAULT_TIME.second)
+    expected_wait = wait_date.replace(hour=DEFAULT_TIME.hour, minute=DEFAULT_TIME.minute, second=DEFAULT_TIME.second)
+    assert test_task['due'] == expected_due
+    assert test_task['wait'] == expected_wait
+
+
+def test_default_time_not_midnight():
+    due_date = NOW.replace(second=1, microsecond=0) + timedelta(days=2)
+    wait_date = NOW.replace(second=1, microsecond=0) + timedelta(days=1)
+    test_task = Task(
+        {
+            "status": "pending",
+            "description": "Super urgent test task",
+            "tags": ["@work"],
+            "modified": NOW.strftime("%Y%m%dT%H%M%SZ"),
+            "entry": NOW.strftime("%Y%m%dT%H%M%SZ"),
+            "due": due_date.strftime("%Y%m%dT%H%M%SZ"),
+            "wait": wait_date.strftime("%Y%m%dT%H%M%SZ"),
+            "uuid": str(uuid.uuid4())
+        }
+    )
+    default_time_hook.main(test_task)
+    assert test_task['due'] == due_date
+    assert test_task['wait'] == wait_date
---
+++ twpm/hooks/default_time_hook.py
@@ -0,0 +1,42 @@
+"""
+Hook to set default timestamps when setting dates.
+"""
+import logging
+from datetime import datetime
+from datetime import time
+
+from taskw.task import Task
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_TIME = time(23, 59, 59)  # Your wanted default time
+
+
+def set_default_time(timestamp: datetime) -> datetime:
+    """
+    Helper function to set the default timestamp for a given datetime.
+
+    :param timestamp:
+    :return: datetime with hour, minute, and second values to set the defaults.
+    """
+    return timestamp.replace(
+        hour=DEFAULT_TIME.hour,
+        minute=DEFAULT_TIME.minute,
+        second=DEFAULT_TIME.second,
+    )
+
+
+def main(task: Task) -> None:
+    """
+    Default time hook entry point.
+
+    :param task: Task instance
+    :return: None
+    """
+    if task['due'] and task['due'].time() == time(0, 0, 0):
+        task['due'] = set_default_time(task['due'])
+        logger.info("Default due time has been set to %s", task['due'])
+
+    if task['wait'] and task['wait'].time() == time(0, 0, 0):
+        task['wait'] = set_default_time(task['wait'])
+        logger.info("Default wait time has been set to %s", task['wait'])
