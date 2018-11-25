f0cdfbcb68e92513493ec73437f285b512f9170e
Fongshway <8451964+Fongshway@users.noreply.github.com>
2018-11-24 21:51:08 -0800

    Update HookRunner run method

--- twpm/hook_runner.py
+++ twpm/hook_runner.py
@@ -12,6 +12,8 @@ from taskw.fields import ArrayField
 from taskw.task import Task
 
 from twpm.hooks import example_hook
+from twpm.hooks import default_time_hook
+from twpm.hooks import inbox_tag_hook
 
 logger = logging.getLogger(__name__)
 
@@ -80,10 +82,13 @@ class HookRunner:
         input_task = self.from_input()
 
         # Run all active hooks
+        # TODO Expose ability to define hooks in .taskrc (e.g. twpm.hooks = inbox_tag_hook,default_time_hook)
         example_hook.main(input_task)
+        inbox_tag_hook.main(input_task)
+        default_time_hook.main(input_task)
 
-        # Export the final task after all active hooks have run
-        logger.info(self.to_output(input_task.serialized()))
+        # Write the final task to stdout after all active hooks have run
+        print(self.to_output(input_task.serialized()))
 
         # Exit
         sys.exit(0)
