18c69fa972be963a8d280b7a063978a13ecafb48
Fongshway <8451964+Fongshway@users.noreply.github.com>
2019-12-01 10:55:19 -0800

    red

--- twpm/extensions/parser.py
+++ twpm/extensions/parser.py
@@ -1,13 +1,28 @@
 """
 Parser for Taskwarrior data.
 """
+import json
+
 from typing.io import IO
 
 
-def parse_timewarrior_data(input_stream: IO) -> dict:
-    # Convert to dict
-    config = 'yo'
-    intervals = 'yo'
-    # Split into config and intervals
+def parse_timewarrior_data(input_stream: str) -> dict:
+    # Extract the configuration settings.
+    header = 1
+    config = dict()
+    body = ""
+    for line in input_stream.split('\n'):
+        if header:
+            if line == "":
+                header = 0
+            else:
+                fields = line.strip().split(": ", 2)
+                if len(fields) == 2:
+                    config[fields[0]] = fields[1]
+                else:
+                    config[fields[0]] = ""
+        else:
+            body += line
 
+    intervals = json.loads(body)
     return config, intervals
