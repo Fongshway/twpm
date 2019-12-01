4d818d54d69eb5f993e75f60050553d3f4641fa2
Fongshway <8451964+Fongshway@users.noreply.github.com>
2019-12-01 11:47:49 -0800

    Use fixtures

---
+++ tests/extensions/conftest.py
@@ -0,0 +1,24 @@
+"""
+Timewarrior extension pytest fixtures.
+"""
+import datetime
+from io import StringIO
+
+import pytest
+
+
+@pytest.fixture
+def filled_database():
+    now = datetime.datetime.now()
+    now_utc = now.utcnow()
+    one_hour_before_utc = now_utc - datetime.timedelta(hours=1)
+
+    input_stream = ''.join([
+        'color: off\n',
+        'debug: on\n',
+        f'temp.report.start: {one_hour_before_utc:%Y%m%dT%H%M%S}Z\n',
+        f'temp.report.end: {now_utc:%Y%m%dT%H%M%S}Z\n',
+        '\n',
+        f'[{{"start":"{one_hour_before_utc:%Y%m%dT%H%M%S}Z","end":"{now_utc:%Y%m%dT%H%M%S}Z","tags":["foo"]}}]',
+    ])
+    return StringIO(input_stream), one_hour_before_utc, now_utc
--- tests/extensions/test_parser.py
+++ tests/extensions/test_parser.py
@@ -1,36 +1,22 @@
 """
 Tests for Taskwarrior data parser.
 """
-import datetime
-from io import StringIO
-
 from twpm.extensions.parser import parse_timewarrior_data
 
 
-def test_parse_timewarrior_data():
-    now = datetime.datetime.now()
-    now_utc = now.utcnow()
-    one_hour_before_utc = now_utc - datetime.timedelta(hours=1)
-
-    input_stream = ''.join([
-        'color: off\n',
-        'debug: on\n',
-        f'temp.report.start: {one_hour_before_utc:%Y%m%dT%H%M%S}Z\n',
-        f'temp.report.end: {now_utc:%Y%m%dT%H%M%S}Z\n',
-        '\n',
-        f'[{{"start":"{one_hour_before_utc:%Y%m%dT%H%M%S}Z","end":"{now_utc:%Y%m%dT%H%M%S}Z","tags":["foo"]}}]',
-    ])
-    config, intervals = parse_timewarrior_data(StringIO(input_stream))
+def test_parse_timewarrior_data(filled_database):
+    input_stream, start, end = filled_database
+    config, intervals = parse_timewarrior_data(input_stream)
     assert config == {
         'color': 'off',
         'debug': 'on',
-        'temp.report.start': f'{one_hour_before_utc:%Y%m%dT%H%M%S}Z',
-        'temp.report.end': f'{now_utc:%Y%m%dT%H%M%S}Z',
+        'temp.report.start': f'{start:%Y%m%dT%H%M%S}Z',
+        'temp.report.end': f'{end:%Y%m%dT%H%M%S}Z',
     }
     assert intervals == [
         {
-            "start": f"{one_hour_before_utc:%Y%m%dT%H%M%S}Z",
-            "end": f"{now_utc:%Y%m%dT%H%M%S}Z",
+            "start": f"{start:%Y%m%dT%H%M%S}Z",
+            "end": f"{end:%Y%m%dT%H%M%S}Z",
             "tags": ["foo"]
         }
     ]
